d6371c026011356aeb549c996c7819c88f00ca93
poojaarule <135432764+poojaarule@users.noreply.github.com>
2024-01-16 04:52:00 +0530

    Add files via upload

---
+++ click.js
@@ -0,0 +1,19 @@
+let counterElement = document.getElementById("counterValue");
+let clickCount = localStorage.getItem("clickCount"); // We are gtting the value from localstorge
+let incrementBtnE1 = document.getElementById("incrementBtn");
+
+if (clickCount === null) {
+    counterElement.textContent = 0; //If localStorage doesnot have a value in it, we are dispalying 0
+} else {
+    counterElement.textContent = clickCount; //If local storage has some value, m ,value
+}
+
+
+function onIncrementCount() {
+    let previousValue = counterElement.textContent;
+    let updateValue = parseInt(previousValue) + 1; //we are getting the displayed value  and incrementing it.
+
+
+    localStorage.setItem("clickCount", updateValue); //We are updating the counter value in local storgae
+    counterElement.textContent = updateValue; //We are displaying the updated value
+}
